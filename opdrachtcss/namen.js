1432d73302447f4c6165f042af95b50c14d58aca
FirasSoekhai <54799485+FirasSoekhai@users.noreply.github.com>
2019-12-09 16:04:47 +0100

    Add files via upload

---
+++ opdrachtcss/namen.js
@@ -0,0 +1,7 @@
+let Joey = document.getElementById("Joey");
+let Firas = document.getElementById("Firas");
+
+let name = ["joey van der kuijl", "Firas Soekhai"];
+
+    Joey.innerHTML = name[0];
+    Firas.innerHTML = name[1];
